56de47caac4c18bc4f5c5f0a527000219c1857ff
Eduardo <picis_9716@hotmail.com>
2026-08-23 17:13:40 -0500

    INVENTARIO V.2.0

--- resources/views/template/nav.blade.php
+++ resources/views/template/nav.blade.php
@@ -17,7 +17,7 @@
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{ route('sales.report.index') }}">Ventas</a>
+                    <a class="nav-link active" aria-current="page" href="{{ route('sales.report.index') }}">Reportes</a>
                 </li>
             </ul>
         </div>
